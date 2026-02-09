c3cf6824f5f179c61df438e0092e5084726c4c56
Muhammad-sobhi <Muhammadsobhi.25@gmail.com>
2026-02-09 20:15:28 +0200

    Update routing logic and live backend URLs

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,21 @@ const nextConfig: NextConfig = {
         port: "8000",
         pathname: "/storage/**",
       },
+      {
+        protocol: "https",
+        hostname: "ready-ally-thattreeguy-134e425d.koyeb.app",
+        pathname: "/storage/**",
+      },
     ],
+  },
+  // This solves the CORS "blocked by policy" errors
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://ready-ally-thattreeguy-134e425d.koyeb.app/api/:path*',
+      },
+    ];
   },
 };
 
